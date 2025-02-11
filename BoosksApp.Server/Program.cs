e3eb11a01b270c438ac14782b688475929ecd2e5
Darlin Ruben Nina C. <78616515+Ruben890@users.noreply.github.com>
2025-02-11 14:47:15 -0400

    .

--- BoosksApp.Server/Program.cs
+++ BoosksApp.Server/Program.cs
@@ -10,7 +10,6 @@ builder.Services.AddHttpClient<ExternalApiRequest>();
 builder.Services.AddScoped<ExternalApiRequest>();
 builder.Services.AddScoped<IBooksServices, BooksServices>();
 
-
 builder.Services.AddControllers(config =>
 {
     config.RespectBrowserAcceptHeader = true;
@@ -23,6 +22,26 @@ builder.Services.AddControllers(config =>
     options.SerializerSettings.NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore;
 });
 
+// Leer la lista de orígenes permitidos desde la configuración
+var allowedOrigins = builder.Configuration.GetValue<string[]>("AllowedOrigins");
+
+if (allowedOrigins is null || allowedOrigins.Length == 0)
+{
+    throw new InvalidOperationException("No allowed origins have been defined in the settings. Make sure to add the 'AllowedOrigins' section in appsettings.");
+}
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("CorsPolicy", builder =>
+    {
+        builder.WithOrigins(allowedOrigins)
+               .AllowAnyHeader() // Permitir todos los encabezados
+               .AllowAnyMethod() // Permitir todos los métodos (GET, POST, PUT, DELETE, etc.)
+               .AllowCredentials() // Habilitar credenciales
+               .WithExposedHeaders("X-Custom-Header") // Exponer solo los encabezados necesarios
+               .SetPreflightMaxAge(TimeSpan.FromMinutes(10));
+    });
+});
 
 var app = builder.Build();
 
@@ -31,6 +50,7 @@ app.UseForwardedHeaders(new ForwardedHeadersOptions
     ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto,
 });
 
+app.UseCors("CorsPolicy");
 app.UseRouting();
 app.MapStaticAssets();
 app.MapControllers();
